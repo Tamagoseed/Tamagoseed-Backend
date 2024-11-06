cd7f4883f145c20a2b2f0a5aa9a2061f4446cbf0
MatheusAlcn <matheusalcantara717@gmail.com>
2024-11-06 19:21:58 -0300

    update

--- src/main/java/com/tamagoseed/TamagoseedApplication.java
+++ src/main/java/com/tamagoseed/TamagoseedApplication.java
@@ -2,12 +2,12 @@ package com.tamagoseed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.tamagoseed.repository")
 public class TamagoseedApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TamagoseedApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TamagoseedApplication.class, args);
+    }
 }
